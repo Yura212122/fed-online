b4444d13482bf60e5b2505b48b4bbc76cce3c50e
Yura212122 <urab1634@gmail.com>
2024-10-22 14:51:39 +0300

    js1

---
+++ JSCOURS/01/main.js
@@ -0,0 +1,20 @@
+let firstnumber = 2
+let secondnumber = 3
+let treenumber = 4
+let fournumber = 5
+let fifenumber = 7
+let sixnumber = 10
+let sevennumber = 15
+  let result = (5 + 3) * 2 - 7;
+  console.log(result);
+  let result2 = (10 / 2) ** 2;
+  console.log(result2);
+  let result3 = 2 * (4 + 3) - (8 / 2);
+  console.log(result3);
+  let result4 = (15 % 4) + 5;
+  console.log(result4);
+  let result5 = 3 - (2 ** 4);
+  console.log(result5);
+  let result6 = 1 + 3 * (3 - 2)  ;
+  console.log(result6);
+ 
