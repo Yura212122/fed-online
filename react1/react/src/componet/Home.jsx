ea2d7ae0b9451b4a5032bb563477e071abec9791
Yura212122 <urab1634@gemail.com>
2025-01-21 14:49:29 +0200

    react12

--- react1/react/src/componet/Home.jsx
+++ react1/react/src/componet/Home.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-const Home = () => {
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import './about.css';
+import { Pagination } from 'swiper/modules';
+ 
+
+ const Home = () => {
     return (
         <div>
             <h1>
                 Home page
-            </h1>
-        </div>
+                <FontAwesomeIcon icon={faStar} />
+            </h1> 
+            <Swiper
+                slidesPerView={3}
+                spaceBetween={30}
+                pagination={{
+                clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>    </SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 5</SwiperSlide>
+                <SwiperSlide>Slide 6</SwiperSlide>
+                <SwiperSlide>Slide 7</SwiperSlide>
+                <SwiperSlide>Slide 8</SwiperSlide>
+                <SwiperSlide>Slide 9</SwiperSlide>
+            </Swiper>
+     </div>
     );
 };
 
